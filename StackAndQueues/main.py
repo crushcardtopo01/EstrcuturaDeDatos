caee5afed647f3ce2cbdc00776ff5cbe27471e47
ctrevera-rsi <silentlv5@gmail.com>
2023-02-21 22:03:32 -0600

    feat: add queue implementation

--- StackAndQueues/main.py
+++ StackAndQueues/main.py
@@ -1,3 +1,24 @@
+import  queues
+
+
 def __main__():
-    print("Welcome to my code!")
-    print("Please choose a option")
+    isActive = True
+    while(isActive):
+        print("Welcome to my code!")
+        print("Please choose a option")
+        print("1.- Queues")
+        print("2.- Stack")
+        print("3.- exit")
+        option = input(" Enter your option")
+
+        if (option == "1"):
+            queues.Queue()
+
+        if (option == "2"):
+            print("something")
+        if (option == "3"):
+            isActive = False
+
+    print("good bye! 🙃 ")
+
+__main__()
--- StackAndQueues/queues.py
+++ StackAndQueues/queues.py
@@ -1,3 +1,42 @@
-def __main__():
-    print("Welcome to my code!")
-__main__()
+
+
+def RemoveElement(queue):
+    _queue = queue
+    if(len(_queue) != 0):
+        #if you dont define a index, pop method remove the last item inside of the list
+        _queue.pop()
+    else:
+        print("\n \nThe queue dont have elements!!")
+    return _queue
+
+def AddElement(queue):
+    _queue = queue
+    newElement = input(" \n \nEnter the new element: ")
+    _queue.append(newElement)
+    return _queue
+
+def ShowQueue(queue):
+    print("XXXXXXXXXX   Queue    XXXXXXXXXXXX")
+    print(queue)
+    print("XXXXXXXXXXXXXXXXXXXXXX\n\n")
+
+def Queue():
+    queue = []
+    print("\n \n\n \nWelcome to Queues!! ")
+    isActive = True
+    while(isActive):
+        print("Choose a option")
+        print("1.- add element")
+        print ("2.- remove element")
+        print("3.- see the queue")
+        print("4.- exit")
+        option = input("enter you option: ")
+        
+        if(option =="1"):
+            queue = AddElement(queue)
+        if(option =="2"):
+            queue = RemoveElement(queue)
+        if(option =="3"):
+            ShowQueue(queue)
+        if(option =="4"):
+            isActive = False
